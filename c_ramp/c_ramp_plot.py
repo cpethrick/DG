e4c31ab004ffa53d7f95de0f918cd4d9800066cc
Carolyn <c.pethrick@gmail.com>
2024-11-19 13:51:27 -0500

    Updates to c ramp test

--- c_ramp/c_ramp_plot.py
+++ c_ramp/c_ramp_plot.py
@@ -18,7 +18,7 @@ import scipy.special as bessel
 plt.style.use('ggplot')
 
 from cycler import cycler
-mycols = cycler(color=['#ED1B2F', '#b9b4b3', '#1896cb','#B2D235', '#C768A9', '#9E0918','#E8B92E', '#9E0918', '#F7941D', '#024F6D'])
+mycols = cycler(color=['#ED1B2F', '#b9b4b3', '#1896cb','#B2D235', '#C768A9', '#F7941D', '#E8B92E', '#9E0918',  '#024F6D'])
                        
 plt.rc('axes', prop_cycle=mycols)
 
@@ -39,33 +39,38 @@ plt.rcParams['text.usetex']=True
 #%%
 
 
-filenames = ["1D_linear_advection_P3_result.csv",
-             "1D_linear_advection_P4_result.csv",
-             "spacetime_linear_advection_P3_result.csv",
-             "spacetime_linear_advection_P4_result.csv",
-             "1D_burgers_P3_result.csv",
-             "1D_burgers_P4_result.csv",
-             "spacetime_burgers_P3_result.csv",
-             "spacetime_burgers_P4_result.csv",
+filenames = ["castonguay_p4.csv","spacetime_linear_advection_P4_result.csv","1D_linear_advection_P4_result.csv", 
+             "castonguay_p3.csv", "spacetime_linear_advection_P3_result.csv","1D_linear_advection_P3_result.csv", 
              ]
 
-labelnames = [fname.replace("_result.csv", "").replace("_"," ") for fname in filenames]
+labelnames = ["Castonguay, $P=4$", "Space-time, $P=4$", "MoL, $P=4$", 
+              "Castonguay, $P=3$", "Space-time, $P=3$", "MoL, $P=3$",
+        ]
 
-(fig,ax) = plt.subplots(1,1,figsize=(4,2.5))
-(fig,ax) = plt.subplots(1,1,figsize=(8,6))
+(fig,ax) = plt.subplots(1,1,figsize=(5,2.5))
 
 datastore = {}
 for fname in filenames:
     datastore[fname] = pd.read_csv(fname)
+    if "castonguay" in fname:
+        # Account for different basis
+        datastore[fname]["c_value"]  /= 2.0
     
-    plt.semilogx(datastore[fname]["c_value"],datastore[fname]["OOA"] )
+    linest = "-"
+    if "castonguay" in fname:
+        linest = ":"
+    elif "1D" in fname:
+        linest = "-."
+    datastore[fname].sort_values("c_value", inplace=True)
+    plt.semilogx(datastore[fname]["c_value"],datastore[fname]["OOA"], linestyle = linest )
     
+plt.xlim([1E-7,1E4])
 plt.ylabel("OOA")
-plt.xlabel("FR c")
+plt.xlabel("c")
 box = ax.get_position()
 #shrink plot
 ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
 #place plot outside box
 plt.legend(labelnames, loc = 'center left', bbox_to_anchor=(1,0.5))
-
-plt.savefig("c_ramp_fig.pdf")
+#plt.legend(labelnames)
+plt.savefig("c_ramp_fig.pdf", bbox_inches='tight')
