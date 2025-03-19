5bba234ebd9bd10694719a6104b8052171d928ab
Carolyn <c.pethrick@gmail.com>
2025-03-19 15:35:27 -0400

    Write entropy evolution to file

--- src/post_processing.jl
+++ src/post_processing.jl
@@ -74,54 +74,42 @@ function calculate_projection_error_local(u_face_interior, u_face_Dirichlet, iel
 end
 
 function calculate_projection_corrected_entropy_change(u_hat, dg::DG, param::PhysicsAndFluxParams)
-    entropy_initial = 0
-    entropy_final = 0
     projection_error = 0
 
+
+    entropy_integration_at_surfaces = zeros(2,dg.N_elem_per_dim)
+    entropy_initial = entropy_integration_at_surfaces[1,1]
+
     for ielem = 1:dg.N_elem
-        if ielem < dg.N_elem_per_dim+1
+        itslab = dg.EIDtoTSID[ielem]
+        u_hat_local = u_hat[(ielem-1)*dg.N_soln_dof+1:(ielem)*dg.N_soln_dof]
+        if itslab == 1
             # face on bottom (t=0)
             
             # get initial condition
             x_local = dg.VX[ielem] .+ 0.5* (dg.r_flux.+1) * dg.delta_x
             y_local = zeros(size(x_local)) # leave zero as this is the lower face
             u_face_Dirichlet = calculate_solution_on_Dirichlet_boundary(x_local, y_local, dg,param)
-            
-            u_hat_local = u_hat[(ielem-1)*dg.N_soln_dof+1:(ielem)*dg.N_soln_dof]
             u_face_interior = project(dg.chi_face[:,:,3],  u_hat_local,true,dg,param)
 
-
             s_vec = get_numerical_entropy_function(u_face_Dirichlet,param)
-            entropy_initial += s_vec' * dg.W_face * dg.J_face * ones(size(s_vec))
-
-
-            # get entropy potential and entropy variables at the face (interior soln)
-            #phi_face_interior = get_entropy_potential(u_face_interior, param)
-            #phi_face_Dirichlet = get_entropy_potential(u_face_Dirichlet, param)
-            #v_face_interior = get_entropy_variables(u_face_interior, param)
-            #v_face_Dirichlet = get_entropy_variables(u_face_Dirichlet, param)
-
-            #phi_jump = phi_face_interior - phi_face_Dirichlet
-            #v_jump = v_face_interior - v_face_Dirichlet
-
-
-            #vjumpTtimesu = zeros(size(phi_face_Dirichlet))
-            #for inode = 1:dg.N_vol_per_dim
-            #    node_indices = dg.N_vol_per_dim*(1:dg.N_state) .- dg.N_vol_per_dim .+ inode
-            #    vjumpTtimesu[inode] += (v_jump[node_indices])' * u_face_Dirichlet[node_indices]
-            #end
-
-
+            entropy_integration_at_surfaces[1,1] += s_vec' * dg.W_face * dg.J_face * ones(size(s_vec))
             projection_error += calculate_projection_error_local(u_face_interior,u_face_Dirichlet, ielem, dg,param)#(phi_jump - vjumpTtimesu)' * dg.W_face * dg.J_face * ones(size(phi_jump))
-        elseif ielem > dg.N_elem_per_dim^dg.dim - dg.N_elem_per_dim
-            # face on top (t=tf)
-            u_hat_local = u_hat[(ielem-1)*dg.N_soln_dof+1:(ielem)*dg.N_soln_dof]
-            u_face_interior = project(dg.chi_face[:,:,4],  u_hat_local,true,dg,param)
-
-            s_vec = get_numerical_entropy_function(u_face_interior,param)
-            entropy_final += s_vec' * dg.W_face * dg.J_face * ones(size(s_vec))
+        else
+            # face on bottom of element
+            u_face_bottom_interior = project(dg.chi_face[:,:,3],  u_hat_local,true,dg,param)
+            s_vec = get_numerical_entropy_function(u_face_bottom_interior,param)
+            entropy_integration_at_surfaces[1,itslab] += s_vec' * dg.W_face * dg.J_face * ones(size(s_vec))
         end
+
+        u_face_top = project(dg.chi_face[:,:,4], u_hat_local, true, dg, param)
+        s_vec = get_numerical_entropy_function(u_face_top,param)
+        entropy_integration_at_surfaces[2,itslab] += s_vec' * dg.W_face * dg.J_face * ones(size(s_vec))
+    
     end
+
+    entropy_initial = entropy_integration_at_surfaces[1,1]
+    entropy_final = entropy_integration_at_surfaces[2,end]
 
     display("Final entropy is ")
     display(entropy_final)
@@ -133,6 +121,16 @@ function calculate_projection_corrected_entropy_change(u_hat, dg::DG, param::Phy
     display(entropy_final - entropy_initial + projection_error)
     display("Without projection correction:")
     display(entropy_final - entropy_initial)
+    if true
+        fname = "entropy_preservation_check.csv"
+        f = open(fname, "w")
+        DelimitedFiles.writedlm(f, [-1 projection_error 0.0], ",")
+        DelimitedFiles.writedlm(f, [dg.VX[1:end] [entropy_integration_at_surfaces[1,:]' NaN]' [NaN entropy_integration_at_surfaces[2,:]']'], ",")
+        close(f)
+
+    end
+
+    display(entropy_integration_at_surfaces)
     return entropy_final - entropy_initial + projection_error #return preservation
 
 end
