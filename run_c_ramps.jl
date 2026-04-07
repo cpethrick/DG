3e3ce0ddbda9eb78f817479064c33fb1d999fea2
Carolyn <c.pethrick@gmail.com>
2026-04-07 14:51:45 -0400

    Remove accidentally commited script files

--- run_c_ramps.jl
+++
@@ -1,8 +0,0 @@
-include("src/main.jl")
-# OOA tests
-c_ramp_test("c_ramp_tests/lin_adv_P2_GLLGL.csv")
-c_ramp_test("c_ramp_tests/lin_adv_P3_GLLGL.csv")
-c_ramp_test("c_ramp_tests/lin_adv_P4_GLLGL.csv")
-# Stability tests
-c_ramp_test("c_ramp_tests/spacetime_burgers_stability_GLLGL_p3.csv", false, true)
-c_ramp_test("c_ramp_tests/spacetime_burgers_stability_GLLGL_p4.csv", false, true)
